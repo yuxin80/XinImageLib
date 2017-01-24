570fd14887ee78edaf69266fa4066a107b6bf7d7
yuxin80 <yuxin80@gmail.com>
2017-01-24 17:54:13 +0800

    111

--- LoadDicom.py
+++ LoadDicom.py
@@ -14,6 +14,10 @@ import numpy
 import os
 import scipy
 
+import matplotlib.pyplot as plt
+from skimage import measure, morphology 
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
 """
 folder = ''
 patients = os.listdir(folder)
@@ -97,3 +101,23 @@ def resample(image, scan, new_spacing=[1,1,1]):
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
     
     return image, new_spacing
+
+def plot_3d(image, threshold=-300):
+    p = image.transpose(2,1,0)
+    p=p[:,:,::-1]
+    
+    verts, faces = measure.marching_cubes(p,threshold)
+    
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111,projection='3d')
+    
+    mesh = Poly3DCollection(verts[faces], alpha=0.1)
+    face_color = [0.5,0.5,1]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+    
+    ax.set_xlim(0,p.shape[0])
+    ax.set_ylim(0,p.shape[1])
+    ax.set_zlim(0,p.shape[2])
+    
+    plt.show()
